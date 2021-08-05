fc44f94bec17ecb39d31919bf86a32cd1154ad5e
joshgare <josh@jgapplications.com>
2021-08-05 11:30:27 +0100

    Create static library

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "FlagPhoneNumber",
+            type: .static,
             targets: ["FlagPhoneNumber"]
         )
     ],
